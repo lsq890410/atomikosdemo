3aba2d0b771308a54c88580a332def634ba4ac6f
lsq890410 <279159029@qq.com>
2018-11-03 19:42:56 +0800

    11

---
+++ AtomikosDemo/src/main/java/com/lisq/atomikos/demo/AtomikosDemoApplication.java
@@ -0,0 +1,20 @@
+package com.lisq.atomikos.demo;
+
+import java.sql.SQLException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
+import com.lisq.atomikos.demo.order.OrderService;
+
+@SpringBootApplication
+@ImportResource("classpath:config.xml")
+public class AtomikosDemoApplication {
+	public static void main(String[] args) throws SQLException {
+		ApplicationContext context = SpringApplication.run(AtomikosDemoApplication.class, args);
+		OrderService orderService = context.getBean(OrderService.class);
+		orderService.addOrder();
+	}
+}
---
+++ AtomikosDemo/src/main/java/com/lisq/atomikos/demo/order/OrderService.java
@@ -0,0 +1,30 @@
+package com.lisq.atomikos.demo.order;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class OrderService {
+	@Autowired
+	private DataSource dataSource1;
+	@Autowired
+	private DataSource dataSource2;
+	@Transactional
+	public void addOrder() throws SQLException {
+		Connection conn1 = dataSource1.getConnection();
+		Connection conn2 = dataSource2.getConnection();
+		Statement statement1 =conn1.createStatement();
+		Statement statement2 =conn2.createStatement();
+		statement1.executeQuery("insert into order(orderno)values('1')");
+		statement2.executeQuery("insert into order(orderno)values('1')");
+		conn1.close();
+		conn2.close();
+	}
+}
